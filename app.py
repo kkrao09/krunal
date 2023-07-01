b74901c07405d08becb3c6891f24c23e5d272da6
Mukesh <kkrao09@gmail.com>
2023-07-01 15:49:46 +0530

    add new route

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 def hello():
     return 'Hello'
 
-# @app.route('/world')
-# def helloWorld():
-#     return 'Hello World'
+@app.route('/world')
+def helloWorld():
+    return 'Hello World'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
